8218bfba9b1a10067a39af6763ce770820780cda
African-C12 <michaeloluigbo12@gmail.com>
2025-01-19 18:44:37 +0000

    Delete Healthcare directory

--- Healthcare/app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Patient
-from .forms import PatientForm 
-
-class PatientForm(forms.ModelForm):
-    class Meta:
-        model = Patient
-        fields = ['Patient_Name', 'Date_of_Birth', 'Email', 'Gender', 'Age', 'Phone', 'Address']    
